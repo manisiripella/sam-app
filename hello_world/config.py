04b40b1d9e19e77cc6154a4e2f595a0178896c35
manisiripella <mani.siripella@gmail.com>
2025-11-22 19:47:12 +0530

    Add initial config.py with process_data function

---
+++ hello_world/config.py
@@ -0,0 +1,8 @@
+# release branch
+# TODO: Remove this later
+# some debug comments from testing
+def process_data():
+    value = compute_value()  # debugging
+    # printing temporarily
+    print("debug")
+    return value
